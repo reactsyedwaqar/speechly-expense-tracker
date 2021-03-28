274bd478f102e7b5066499d8477855cf6176c1c3
reactsyedwaqar <react.syedwaqar@gmail.com>
2021-03-28 00:27:27 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef } from 'react';
+import { Grid } from '@material-ui/core';
+
+import { SpeechState, useSpeechContext } from "@speechly/react-client";
+import { PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui';
+
+import { Details, Main } from './components';
+import useStyles from './styles';
+
+const App = () => {
+
+
+  return (
+    <div>
+      HI all
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
+
+import { Provider } from './context/context';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
